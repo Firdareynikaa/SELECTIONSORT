3fa42f3df8802cef3d7439828d7b462f23599c3a
Firdareynikaa <firda_reinika_26rpl@student.smktelkom-mlg.sch.id>
2019-07-22 15:01:09 +0700

    selesai

---
+++ SELECTION SORT/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+namespace SELECTION_SORT
+{
+    class Program
+    {
+        static void Main()
+        {
+            int N, S, P; ForegroundColor = ConsoleColor.Yellow;
+            Write("\n Jumlah Data = ");
+            N = int.Parse(ReadLine());
+            int[] A = new int[N];
+            Random R = new Random();
+            for (int X = 0; X < N; X++)
+            { A[X] = R.Next(100, 1000); Write(" {0} ", A[X]); }
+
+            for (int X = 0; X < N - 1; X++)
+            {
+                S = X; P = X;
+                for (int Y = X + 1; Y < N; Y++)
+                    if (A[P] > A[Y]) P = Y;
+
+                if (S != P)
+                { int C = A[S]; A[S] = A[P]; A[P] = C; }
+            }
+
+            WriteLine(); ForegroundColor = ConsoleColor.Green;
+            for (int X = 0; X < N; X++) Write(" {0} ", A[X]);
+            ReadKey();
+        }
+    }
+}
